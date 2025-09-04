2139bfe6f54546e7b63d949cbab2592794f5efc3
Ashish Kumar <ashishrj08mehta@gmail.com>
2025-09-04 09:34:26 +0530

    library loaded

---
+++ knn_digits.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import pandas as pd
+from sklearn.datasets import load_digits
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import classification_report, accuracy_score, confusion_matrix
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+
